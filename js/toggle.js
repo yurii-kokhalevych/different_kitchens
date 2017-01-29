e09caaab15c00b4d6a15c4c2fbe881ea5f23d0b2
Yura Kokhalevych <borodatuu@gmail.com>
2017-01-29 19:49:50 +0200

    fist_commit

---
+++ js/toggle.js
@@ -0,0 +1,96 @@
+ $(document).ready(function(){
+$('.read1').on('click', function(){
+  $(".readmore1").show();
+});
+$('.read1').on('click', function(){
+  $(".zgor1").show();
+});
+$('.read1').on('click', function(){
+  $(".read1").hide();
+});
+
+$('.zgor1').on('click', function(){
+  $(".readmore1").hide();
+});
+
+$('.zgor1').on('click', function(){
+  $(".read1").show();
+});
+
+$('.zgor1').on('click', function(){
+  $(".zgor1").hide();
+});
+});
+
+  $(document).ready(function(){
+$('.read2').on('click', function(){
+  $(".readmore2").show();
+});
+$('.read2').on('click', function(){
+  $(".zgor2").show();
+});
+$('.read2').on('click', function(){
+  $(".read2").hide();
+});
+
+$('.zgor2').on('click', function(){
+  $(".readmore2").hide();
+});
+
+$('.zgor2').on('click', function(){
+  $(".read2").show();
+});
+
+$('.zgor2').on('click', function(){
+  $(".zgor2").hide();
+});
+});
+
+   $(document).ready(function(){
+$('.read3').on('click', function(){
+  $(".readmore3").show();
+});
+$('.read3').on('click', function(){
+  $(".zgor3").show();
+});
+$('.read3').on('click', function(){
+  $(".read3").hide();
+});
+
+$('.zgor3').on('click', function(){
+  $(".readmore3").hide();
+});
+
+$('.zgor3').on('click', function(){
+  $(".read3").show();
+});
+
+$('.zgor3').on('click', function(){
+  $(".zgor3").hide();
+});
+});
+
+    $(document).ready(function(){
+$('.read4').on('click', function(){
+  $(".readmore4").show();
+});
+$('.read4').on('click', function(){
+  $(".zgor4").show();
+});
+$('.read4').on('click', function(){
+  $(".read4").hide();
+});
+
+$('.zgor4').on('click', function(){
+  $(".readmore4").hide();
+});
+
+$('.zgor4').on('click', function(){
+  $(".read4").show();
+});
+
+$('.zgor4').on('click', function(){
+  $(".zgor4").hide();
+});
+});
+
